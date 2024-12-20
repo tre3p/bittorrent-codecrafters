29404e3be2d24a31f10a32b2ec9a74c9fbd7f573
tre3p <whereareyo@yahoo.com>
2024-12-20 16:46:30 +0400

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,10 @@
-import com.google.gson.Gson
-
-val gson = Gson()
-
 fun main(args: Array<String>) {
     val command = args[0]
     when (command) {
         "decode" -> {
             val bencodedValue = args[1]
             val decoded = decodeBencode(bencodedValue)
-            println(gson.toJson(decoded))
+            println(decoded)
             return
         }
         else -> println("Unknown command $command")
@@ -20,7 +16,12 @@ fun decodeBencode(bencodedString: String): String {
         Character.isDigit(bencodedString[0]) -> {
             val firstColonIndex = bencodedString.indexOfFirst { it == ':' }
             val length = Integer.parseInt(bencodedString.substring(0, firstColonIndex))
-            return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length)
+            return "\"${bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length)}\""
+        }
+
+        bencodedString[0].toChar() == 'i' -> {
+            val intEndIdx = bencodedString.indexOfFirst { it == 'e' }
+            return bencodedString.substring(1, intEndIdx)
         }
         else -> TODO("Only strings are supported at the moment")
     }
